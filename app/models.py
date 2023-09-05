070cbda3fadea09a2fdf725d0c126432022cd067
Alfie Martin <alfiemartin01@gmail.com>
2023-09-05 09:21:35 +0100

    Added Transaaction table model

--- app/models.py
+++ app/models.py
@@ -9,4 +9,9 @@ class Customer(db.Model):
     address_line = db.Column(db.String(30))
     city = db.Column(db.String(30))
     postcode = db.Column(db.String(8))
-    customer = db.relationship('Transaction',backref="transactionbr")
+    customer = db.relationship('Transaction',backref="customerbr")
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    cust_id = db.Column(db.Integer,db.ForeignKey('customer.id'),nullable=False)
+    booking = db.relationship('Booking',backref='transactionbr')
