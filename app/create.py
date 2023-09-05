bda45b99e2f870fb8022992afc7e71a3f743f4a4
jhard <j.harding26@outlook.com>
2023-09-05 09:51:13 +0100

    Removed booking relationship in transaction (unnecessary). Changed relationship name in customer to transaction.

--- app/create.py
+++ app/create.py
@@ -3,13 +3,18 @@ from models import *
 
 actor = Actor(first_name="John", last_name="actor")
 film = Film(title="My first film.")
+customer = Customer(username="John Buyer", password="Password")
+transaction = Transaction(customer=customer)
+
+
 film.actors.append(actor)
+
 
 def populate_db():
     with app.app_context():
         db.drop_all()
         db.create_all()
-        db.session.add_all([actor, film])
+        db.session.add_all([actor, film, customer, transaction])
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -30,10 +30,9 @@ class Customer(db.Model):
     address_line = db.Column(db.String(30))
     city = db.Column(db.String(30))
     postcode = db.Column(db.String(8))
-    customer = db.relationship('Transaction', backref="customer")
+    transactions = db.relationship("Transaction", backref="customer")
 
 
 class Transaction(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    cust_id = db.Column(db.Integer,db.ForeignKey('customer.id'),nullable=False)
-    booking = db.relationship('Booking',backref='transactionbr')
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
