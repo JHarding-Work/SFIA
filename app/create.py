6fed4821169cbc1bf8c0353d5d0cf58fe94ab901
jhard <j.harding26@outlook.com>
2023-09-05 09:38:47 +0100

    Added Actor and Film model.

--- app/create.py
+++ app/create.py
@@ -1,11 +1,15 @@
 from app import app, bcrypt
 from models import *
 
+actor = Actor(first_name="John", last_name="actor")
+film = Film(title="My first film.")
+film.actors.append(actor)
 
 def populate_db():
     with app.app_context():
         db.drop_all()
         db.create_all()
+        db.session.add_all([actor, film])
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -1,4 +1,31 @@
 from app import db
 
 
+actor_film = db.Table(
+    "actor_film",
+    db.metadata,
+    db.Column("actor_id", db.ForeignKey("actor.id")),
+    db.Column("film_id", db.ForeignKey("film.id"))
+)
+
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    films = db.relationship("Film", secondary="actor_film", back_populates="actors")
+
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    description = db.Column(db.Text)
+    actors = db.relationship("Actor", secondary="actor_film", back_populates="films")
+
+
+
+
+
+
+
 # Models go here.
