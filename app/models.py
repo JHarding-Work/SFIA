3bb128a0870c94e781163621535d17c6f361ff4c
Alfie Martin <alfiemartin01@gmail.com>
2023-09-05 09:18:22 +0100

    Added Customers table model

--- app/models.py
+++ app/models.py
@@ -2,3 +2,11 @@ from app import db
 
 
 # Models go here.
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    address_line = db.Column(db.String(30))
+    city = db.Column(db.String(30))
+    postcode = db.Column(db.String(8))
+    customer = db.relationship('Transaction',backref="transactionbr")
