f0831b0ef4f5d81d65e80a3c843cffbd6dca36ef
Alfie Martin <alfiemartin01@gmail.com>
2023-09-08 11:10:29 +0100

    added Validation errors to signup page

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,10 @@
 import os
 
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField, SelectField, IntegerField
-from wtforms.validators import DataRequired,Length,ValidationError
+from wtforms.validators import DataRequired,Length, ValidationError
+from app.models import Customer
 
 
 class LoginForm(FlaskForm):
@@ -16,19 +18,21 @@ class SignUpForm(FlaskForm):
     password = StringField('Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField('Sign up')
 
-    ##### Fix this Later #####
-#    def validate_password(self, password):
-#
-#            special_char = ['!','£','$','%','^','&','*','(',')',';',':']
-#            digits = list(i for i in range(0,10))
-#
-#            for i in special_char:
-#                if not(i in password.data):
-#                    raise ValidationError(f"Password must include one special character from {special_char}")
-#
-#            for i in digits:
-#                if not(digits in password.data):
-#                    raise ValidationError("Password must include at least one number")
+    def validate_username(self, username):
+        customers = Customer.query.all()
+
+        if any(i.username == username.data for i in customers):
+            raise ValidationError(message="Username is taken, please try a different one.")
+
+    def validate_password(self, password):
+
+            special_char = ['!','£','$','%','^','&','*','(',')',';',':']
+            digits = list(i for i in range(0,10))
+
+            if not any(i in password.data for i in special_char):
+                raise ValidationError(message=f"Password must include one special character from {special_char}")
+            elif not any(str(i) in password.data for i in digits):
+                raise ValidationError(message="Password must include at least one number")
 
 
 class DateSelectForm(FlaskForm):
@@ -37,12 +41,12 @@ class DateSelectForm(FlaskForm):
 
 
 class BookingForm(FlaskForm):
-     movie = SelectField("Movie: ")
+     movie = SelectField("Movie: ", validators=[DataRequired()])
      search = SubmitField("Search")
-     date = DateField("Date")
-     time = SelectField("Times: ")
-     username = StringField("Username: ")
-     password = StringField("Password: ")
-     no_of_adult = IntegerField("Number of Adult tickets")
-     no_of_child = IntegerField("Number of Child tickets")
+     date = DateField("Date", validators=[DataRequired()])
+     time = SelectField("Times: ", validators=[DataRequired()])
+     username = StringField("Username: ", validators=[DataRequired()])
+     password = StringField("Password: ", validators=[DataRequired()])
+     no_of_adult = IntegerField("Number of Adult tickets", validators=[DataRequired()])
+     no_of_child = IntegerField("Number of Child tickets", validators=[DataRequired()])
      submit = SubmitField("Confirm Order")
