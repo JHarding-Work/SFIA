eccfdf3875285bc9872d03260d33c8f954181728
jhard <j.harding26@outlook.com>
2023-09-08 14:43:23 +0100

    Back to mysql.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{os.getenv("MYSQL_ROOT_PASSWORD")}@database:3306/flask-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:Password@database:3306/flask-db'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
