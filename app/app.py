1e0ecea3b50af6707610f4f750d49636384850cb
Ubuntu <ubuntu@ip-172-31-3-65.eu-west-2.compute.internal>
2023-09-10 21:45:33 +0000

    Added wait before server starts up (mysql database needs to load)

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from time import sleep
 
 app = Flask(__name__)
 
@@ -13,7 +14,10 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 from routes import *
+from create import populate_db
 
 
 if __name__ == "__main__":
+    sleep(30)
+    populate_db()
     app.run(host="0.0.0.0", port=5000, debug=True)
