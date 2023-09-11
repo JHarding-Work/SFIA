763c085fb2be0e0feab7ffa33b2c3f55c5a58536
jhard <j.harding26@outlook.com>
2023-09-11 11:23:03 +0100

    Moved tests to folder inside app.

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -3,7 +3,7 @@ from tests import TestBase
 from models import *
 
 from datetime import date, time
-from flask import url_for
+
 
 class TestFilm(TestBase):
     def create_app(self):
@@ -58,61 +58,3 @@ class TestPerson(TestBase):
     def test_fullname(self):
         self.assertEqual("Sarah Performer", self.sarah.fullname)
 
-
-
-class TestViews(TestBase):
-    def test_home_get(self):
-        response = self.client.get(url_for('home'))
-        self.assertEqual(response.status_code, 200)  
-    
-    def test_login_get(self):
-        response = self.client.get(url_for('login'))
-        self.assertEqual(response.status_code, 200)  
-        
-    def test_signup_get(self):
-        response = self.client.get(url_for('signup'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_login_get(self):
-        response = self.client.get(url_for('login'))
-        self.assertEqual(response.status_code, 200)  
-          
-    def test_opening_times_get(self):
-        response = self.client.get(url_for('opening_times'))
-        self.assertEqual(response.status_code, 200)  
-
-    def test_listings_get(self):
-        response = self.client.get(url_for('listings'))
-        self.assertEqual(response.status_code, 200)  
-
-    def test_login_get(self):
-        response = self.client.get(url_for('login'))
-        self.assertEqual(response.status_code, 200)  
-    
-    def test_about_us_get(self):
-        response = self.client.get(url_for('about_us'))
-        self.assertEqual(response.status_code, 200) 
-
-    def test_contacts_get(self):
-        response = self.client.get(url_for('contacts'))
-        self.assertEqual(response.status_code, 200) 
-
-    def test_new_releases_get(self):
-        response = self.client.get(url_for('new_releases'))
-        self.assertEqual(response.status_code, 200)  
-
-    def test_ticket_booking_get(self):
-        response = self.client.get(url_for('ticket_booking'))
-        self.assertEqual(response.status_code, 200)  
-    
-    def test_single_film_get(self):
-        response = self.client.get('/film/1')
-        self.assertEqual(response.status_code, 200)
-    
-           
-           
-          
-          
-
-          
-                  
