0bd6015b4e92c2393ab9fc7fe9dc2042e20e5272
jhard <j.harding26@outlook.com>
2023-09-12 12:40:43 +0100

    Added default to supplied uri environmental variable.

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 mysql_root_password = os.getenv('MYSQL_ROOT_PASSWORD')
 mysql_database_uri = f'mysql+pymysql://root:{mysql_root_password}@mysql:3306/flask-db'
 
-supplied_uri = os.getenv('SQLALCHEMY_DATABASE_URI')
+supplied_uri = os.getenv('SQLALCHEMY_DATABASE_URI', "sqlite:///db.sqlite")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = mysql_database_uri if mysql_root_password else supplied_uri
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
