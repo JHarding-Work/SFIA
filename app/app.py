bfdb8a5b997473882b14cf9b19c43904f17467b5
jhard <j.harding26@outlook.com>
2023-09-14 15:36:25 +0100

    Removed populate action from startup.

--- app/app.py
+++ app/app.py
@@ -18,8 +18,6 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 from routes import *
-from create import populate_with_retries
 
 if __name__ == "__main__":
-    populate_with_retries(10)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
